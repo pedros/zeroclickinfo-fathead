ab597470a95ff0193c33d535f2a9310eca137cb3
Robert Picard <mail@robert.io>
2012-08-02 19:59:59 -0400

    Add the first draft of a refactored fathead (arch_pkgs)

---
+++ lib/DDG/Fathead/ArchPackages.pm
@@ -0,0 +1,26 @@
+package DDG::Fathead::ArchPackages;
+
+# ABSTRACT: Get information about Arch Linux packages
+
+use DDG::Fathead;
+
+source_info (
+    name    => 'Arch Linux Packages',
+    domain  => 'archlinux.org',
+    type    => 'Arch Package',
+);
+
+keywords [
+    'arch',
+    'arch package',
+    'arch linux',
+    'arch linux package',
+    'pacman',
+];
+
+
+attribution (
+    
+);
+
+1;
